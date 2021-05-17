28571f6d2cccb9a518499f8ace4d1cbb2a105961
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 20:54:09 +0530

    Add files via upload

---
+++ Numeric/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace String_format_Numeric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 345672832;
+            double db = 127.345;
+            Console.WriteLine("Number: {0:N}", db);
+            Console.WriteLine("Scientific: {0:E}", db);
+            Console.WriteLine("Currency: {0:C}", db);
+            Console.WriteLine("Percent: {0:P}", db);
+            Console.WriteLine("Hexadecimal: {0:X}", a);
+        }
+    }
+}
