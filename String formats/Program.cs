f8f3eeee8db9ef9b721f0d75e370ddd1d5d9a57e
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 20:52:16 +0530

    Add files via upload

---
+++ String formats/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace String_Formats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp = 20.4;
+            string s = String.Format("The temperature is {0}°C.", temp);
+            Console.WriteLine(s);
+
+            string msg = string.Format("There are {0} owls", 5);
+            Console.WriteLine(msg);
+
+            Console.WriteLine("There are {0} eagles", 3);
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendFormat("There are {0} hawks", 4);
+            Console.WriteLine(builder);
+
+        }
+    }
+
+}
