94e1ee69266a51e74f6eb94bf73699bda987fdf1
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 20:58:47 +0530

    Add files via upload

---
+++ Indexes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace String_Formats_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oranges = 2;
+            int apples = 4;
+            int bananas = 3;
+
+            string str1 = String.Format("There are {0} oranges, {1} apples and {2} bananas", oranges, apples, bananas);
+            string str2 = String.Format("There are {1} oranges, {2} bananas and {0} apples", apples, oranges, bananas);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+
+            Console.WriteLine(str1, oranges, apples, bananas);
+            Console.WriteLine(str2, apples, oranges, bananas);
+        }
+    }
+}
